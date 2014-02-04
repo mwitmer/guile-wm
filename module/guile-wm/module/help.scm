1872d1066e6647967b4a161072fb44c2382f680d
Mark Witmer <mark.d.witmer@gmail.com>
2014-02-03 22:07:17 -0500

    Show command arguments and their types in the help commands

--- module/guile-wm/module/help.scm
+++ module/guile-wm/module/help.scm
@@ -5,18 +5,42 @@
 
 (use-wm-modules message menu)
 
+(define (format-type type)
+  (case type
+    ((#:string) "string")
+    ((#:symbol) "symbol")
+    ((#:number) "number")))
+
+(define (format-arg arg)
+  (format #f "(^(color cyan)~a^(color white): ^(color salmon)~a^(color white)) "
+          (car arg) (format-type (cdr arg))))
+
+(define (format-args args)
+  (if (list? args)
+      (apply string-append (map format-arg args))
+      (format-arg args)))
+
+(define (command-description c)
+  (define summary (command-summary c))
+  (format #f "~a\n~a" summary (command-documentation c)))
+
+(define (command-summary c)
+  (format #f "^(color wheat)~a^(color white) ~a" 
+          (symbol->string c)
+          (format-args (hashq-ref commands c))))
+
 (define-command (help)
   "List all the commands and show documentation for the one selected
 by the user"
   (menu "Select a command:"
-        (map (lambda (c) (cons (symbol->string c) (command-documentation c)))
+        (map (lambda (c)
+               (cons (symbol->string c)
+                     (command-description c)))
              (hash-map->list (lambda (k v) k) commands))
         (lambda (doc)
-          (if doc (message doc) (message "Not documented")))))
+          (if doc (sticky-message doc) (message "Not documented")))))
 
 (define-command (document (cmd #:symbol))
   "Display docstring for command CMD"
-  (or
-   (and=> (command-documentation cmd) message)
-   (message (format #f "No command with name ~a" cmd))))
+  (sticky-message (command-description cmd)))
 
